0dec3702093e78c87e47d16ca309cee2113b73b6
inkcherry <344119449@qq.com>
2018-03-02 23:28:16 +0800

    erode

--- Image-Characteristic-Extraction/src/morph.cpp
+++ Image-Characteristic-Extraction/src/morph.cpp
@@ -14,7 +14,66 @@ kernel_mat(nullptr)//默认
 	kernel_mat = new kernel(default_kernel,3,3);
 }
 
-double ** morph::convolution()//因为膨胀腐蚀  核一般需要 平坦  因此这个地方需要重写
+Mat * morph::morph_operate(bool is_erode)  //
 {
+	double ** res_mat;
+	//默认核是平坦的 
+	if (kernel_mat->main_config.first != kernel_mat->main_config.second) //方形检测
+		exit(-1);
 	
+	if (kernel_mat->main_config.first % 2 != 1)
+		exit(-1);
+	
+	const int size = kernel_mat->main_config.first;
+	const int edge = size / 2;   //floor
+
+	const int bottom = height - size - 1;
+	const int right = width - size - 1;
+
+	if (!is_erode)
+	{ 
+		for (int i = edge; i < height-edge; i++)  //中心处理
+		{
+			double t_max = 0;
+			for (int j = edge; j < width-edge; j++)
+			{
+				int l = i - 1, r = i + 1, int t = j - 1, b = j + 1;
+				for(int k=t;k<b;k++)  
+					for (int s = l; s < r; s++)
+					{
+						if ((*main_mat)[k][s] > t_max)
+							t_max = (*main_mat)[k][s];
+					}
+			}
+		}
+
+		for (int i = 0; i < edge; i++)  //边缘处理(上)
+		{
+			for (int j = 0; j < width; j++);
+		}
+		
+		for (int i = height - edge; i < height-1; i++)//边缘处理(下)
+		{
+			for (int j = 0; j < width; j++);
+		}
+
+		for (int i = 0; i < height; i++)      //(左)
+		{
+			for (int j = 0; j < edge; j++);
+		}
+
+		for (int i = 0; i < height; i++)  //(右)
+		{
+			for (int j = width - edge; j < width; j++);
+		}
+
+	}
+
+	else
+	{
+
+	}
+
+	return nullptr;
 }
+
--- Image-Characteristic-Extraction/src/morph.h
+++ Image-Characteristic-Extraction/src/morph.h
@@ -8,7 +8,6 @@ class morph     //考虑 把filter和morph写一个父类
 	explicit morph(Mat* &main_mat_r, double **kernel);
 	Mat *erode(const int& size, const double &element) { morph_operate(true); };
 	Mat *dilate(const int & size, const double &element) { morph_operate(false); };
-	double**  convolution();
 	//核不应当作为成员 但是已经踩了坑 要微笑这继续踩下去
 
 
